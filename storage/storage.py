2df32ea701bb8e9c137d1544ad0516822fcf2387
Matthew Miller <mnmiller1@me.com>
2017-10-28 16:56:12 +1000

    Fixed user storage.

--- storage/storage.py
+++ storage/storage.py
@@ -27,5 +27,9 @@ def save():
         f.write(data)
 
 
+def add_store(id, fb_access, fb_user):
+    store[id] = {'fb_access': fb_access, 'fb_user': fb_user}
+
+
 def get_id(id):
     return store[id]
