f7a9abd98d04641c3cafed9a7efa51b6eddeae52
Matthew Miller <mnmiller1@me.com>
2017-10-28 12:59:42 +1000

    Added facebook auth hook

--- index.py
+++ index.py
@@ -1,6 +1,28 @@
+import json
+
 from flask import Flask
+
 import routes
+
 app = Flask(__name__)
+
+user_storage = dict()  # This is basically MongoDB, right?
+
+
+def load():
+    with open('user_storage.dat', 'w+') as f:
+        data = f.read()
+        global user_storage  # I always feel so dirty when I do this
+        try:
+            user_storage = json.loads(data)
+        except ValueError:
+            user_storage = dict()
+
+
+def save():
+    data = json.dumps(user_storage)
+    with open('user_storage.dat', 'w+') as f:
+        f.write(data)
 
 
 @app.route('/hook', methods=["POST"])
@@ -8,5 +30,11 @@ def hook():
     return routes.hook()
 
 
+@app.route('/fb_auth', methpds=['GET'])
+def fb_auth():
+    return routes.hecking_facebook_auth()
+
+
 if __name__ == "__main__":
+    load()
     app.run(port=3000)
