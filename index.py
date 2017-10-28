9c91862026cdd3bfc7874e8b1e68d28612a3d899
Bennett Hardwick <bennetthardwick@icloud.com>
2017-10-28 11:56:05 +1000

    Created routes file

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import routes
 app = Flask(__name__)
 
 
 @app.route('/hook', methods=["POST"])
 def hook():
-    return "Hello world!"
+    return routes.hook()
 
 
 if __name__ == "__main__":
