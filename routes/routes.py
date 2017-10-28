a3cc1e59a266c62cb79574fff35220aff5cc5d8c
Bennett Hardwick <bennetthardwick@icloud.com>
2017-10-28 13:11:23 +1000

    Check for webhook type

--- routes/routes.py
+++ routes/routes.py
@@ -1,1 +1,21 @@
-pass
+from flask import request
+import requests
+
+
+def hook():
+    if request.json['payload']['topic'] == 'clockin.updated':
+        return clockin(request.json)
+    elif request.json['payload']['topic'] == 'clockin.photo':
+        return photo(request.json)
+
+
+def clockin(req):
+    # Use the tinder api to find the person's song and
+    # post a request to Byron's server with the name of the song
+    # requests.post('http://localhost:12345', data = {'song':'value'})
+    return "clockin"
+
+
+def photo(req):
+    # Do nothing at all
+    return "photo"
