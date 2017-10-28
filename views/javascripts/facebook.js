5c90597cda391b754aa6130d3911a0a5901172c1
Monique <moniquewinkjames@outlook.com>
2017-10-28 14:57:09 +1000

    Facebook ready to link with Tanda credentials

--- views/javascripts/facebook.js
+++ views/javascripts/facebook.js
@@ -1,11 +1,23 @@
 /**
  * Facebook login
  */
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function send() {
+    //var usercode = getParameterByName('id');
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
     var data = {
+        //"usercode": usercode,
         "username": username,
         "password": password
     };
